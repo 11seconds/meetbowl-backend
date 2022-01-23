9c019c04d5814e91faef2936c730762106443501
Dongwoo Kim <andanend2847@gmail.com>
2022-01-23 22:52:28 +0900

    feat: timetable schema 추가

---
+++ backend/backend/app/app/schemas/timetable.py
@@ -0,0 +1,34 @@
+from pydoc import describe
+from typing import Optional
+
+from pydantic import BaseModel
+
+
+# Shared properties
+class TimeTableBase(BaseModel):
+    id: str
+    title: str
+    description: Optional[str] = None
+    create_user_id: int
+
+
+#업데이트시 받을 데이터
+class TimeTableUpdate(TimeTableBase):
+    id: str
+    create_user_id: str
+    title: str
+    description: Optional[str]
+
+
+# 생성시 받을 데이터
+class TimeTableCreate(TimeTableBase):
+    title: str
+    description: Optional[str]
+    create_user_id: str
+    
+
+# API 반환 데이터
+class TimeTable(TimeTableBase):
+    pass
+    class Config:
+        orm_mode = True
