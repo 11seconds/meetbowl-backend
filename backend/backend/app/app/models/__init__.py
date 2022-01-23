5cf9ae7e81dd7f9740b548e9a56447544d42256d
Dongwoo Kim <andanend2847@gmail.com>
2022-01-23 22:51:24 +0900

    feat: timetable, scheduleblock 모델 추가

--- backend/backend/app/app/models/__init__.py
+++ backend/backend/app/app/models/__init__.py
@@ -1,2 +1,4 @@
 from .item import Item
 from .user import User
+from .timetable import TimeTable
+from .scheduleblock import ScheduleBlock
---
+++ backend/backend/app/app/models/scheduleblock.py
@@ -0,0 +1,21 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
+from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+from app.db.base_class import Base
+
+if TYPE_CHECKING:
+    from .scheduleblock import ScheduleBlock  # noqa: F401
+
+
+class ScheduleBlock(Base):
+    id = Column(String, primary_key=True, index=True)
+    table_id = Column(String, ForeignKey("timetable.id"))
+    user_id = Column(Integer, ForeignKey("user.id"))
+    start_datetime = Column(DateTime)
+    end_datetime = Column(DateTime)
+    label = Column(String)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    timetable = relationship("TimeTable", back_populates="scheduleblocks")
