7895492887ac5ed4e1b8dd94e672ff5a07b69c54
Dongwoo Kim <andanend2847@gmail.com>
2022-01-23 22:54:46 +0900

    feat: timetable, scheduleblock db model 모듈화 추가

--- backend/backend/app/app/db/base.py
+++ backend/backend/app/app/db/base.py
@@ -3,3 +3,5 @@
 from app.db.base_class import Base  # noqa
 from app.models.item import Item  # noqa
 from app.models.user import User  # noqa
+from app.models.timetable import TimeTable  # noqa
+from app.models.scheduleblock import ScheduleBlock  # noqa
