14fa518a21c321aecaf63ba64bb2bb1578f3090f
Dongwoo Kim <andanend2847@gmail.com>
2022-01-25 01:37:33 +0900

    feat: scheduleblock crud 구현 추가

---
+++ backend/backend/app/app/crud/crud_scheduleblock.py
@@ -0,0 +1,35 @@
+from pydoc import describe
+from typing import Any, Dict, Optional, Union, List
+from sqlalchemy import table
+
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.scheduleblock import ScheduleBlock
+from app.schemas.scheduleblock import ScheduleBlockCreate, ScheduleBlockUpdate
+
+from uuid import uuid4
+
+
+class CRUDScheduleblock(CRUDBase[ScheduleBlock, ScheduleBlockCreate, ScheduleBlockUpdate]):
+    def create(self, db: Session, *, obj_in: ScheduleBlockCreate) -> ScheduleBlock:
+        db_obj = ScheduleBlock(
+            id= str(uuid4()),
+            table_id=obj_in.table_id,
+            user_id=obj_in.user_id,
+            start_datetime=obj_in.start_datetime,
+            end_datetime=obj_in.end_datetime,
+            label=obj_in.label
+        )
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+    
+    
+    def get_all(self, db:Session, table_id:str):
+        db_obj = db.query(self.model).filter(self.model.table_id==table_id).all()
+        return db_obj
+
+
+scheduleblock = CRUDScheduleblock(ScheduleBlock)
