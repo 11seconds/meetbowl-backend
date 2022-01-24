4a32cd4ac633ccc420208f11a1ad656d4e92d24e
Dongwoo Kim <andanend2847@gmail.com>
2022-01-25 01:36:58 +0900

    feat: scheduleblock 스키마 생성

--- backend/backend/app/app/schemas/__init__.py
+++ backend/backend/app/app/schemas/__init__.py
@@ -3,3 +3,4 @@ from .msg import Msg
 from .token import Token, TokenPayload
 from .user import User, UserCreate, UserInDB, UserUpdate
 from .timetable import TimeTable, TimeTableCreate, TimeTableUpdate
+from .scheduleblock import ScheduleBlock, ScheduleBlockCreate, ScheduleBlockUpdate
