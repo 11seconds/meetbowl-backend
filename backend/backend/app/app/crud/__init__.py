88ad4c73834156e1f9f34a1c27cd64fcdf6876c5
Dongwoo Kim <andanend2847@gmail.com>
2022-01-25 01:37:33 +0900

    feat: scheduleblock crud 구현 추가

--- backend/backend/app/app/crud/__init__.py
+++ backend/backend/app/app/crud/__init__.py
@@ -1,6 +1,7 @@
 from .crud_item import item
 from .crud_user import user
 from .crud_timetable import timetable
+from .crud_scheduleblock import scheduleblock
 
 # For a new basic set of CRUD operations you could just do
 
