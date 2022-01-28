c6877ac0b428196314fbfc5914e32698724a3ea9
Dongwoo Kim <andanend2847@gmail.com>
2022-01-29 00:01:48 +0900

    feat: add Code schema
    카카오 로그인 구현을 위한 code 스키마 타입 추가

--- backend/backend/app/app/schemas/__init__.py
+++ backend/backend/app/app/schemas/__init__.py
@@ -1,6 +1,6 @@
 from .item import Item, ItemCreate, ItemInDB, ItemUpdate
 from .msg import Msg
-from .token import Token, TokenPayload
+from .token import Token, TokenPayload, Code
 from .user import User, UserCreate, UserInDB, UserUpdate
 from .timetable import TimeTable, TimeTableCreate, TimeTableUpdate
 from .scheduleblock import ScheduleBlock, ScheduleBlockCreate, ScheduleBlockUpdate
