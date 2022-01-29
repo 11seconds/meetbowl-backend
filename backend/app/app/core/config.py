91b5b1d283f1eb7e7145b2b50f699bf27b3a4867
Dongwoo Kim <andanend2847@gmail.com>
2022-01-29 23:00:52 +0900

    refactor: config to use kakao key

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -7,6 +7,7 @@ from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, v
 class Settings(BaseSettings):
     API_V1_STR: str = "/api/v1"
     SECRET_KEY: str = secrets.token_urlsafe(32)
+    KAKAO_APP_KEY: str
     # 60 minutes * 24 hours * 8 days = 8 days
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60 * 24 * 8
     SERVER_NAME: str
@@ -15,6 +16,13 @@ class Settings(BaseSettings):
     # e.g: '["http://localhost", "http://localhost:4200", "http://localhost:3000", \
     # "http://localhost:8080", "http://local.dockertoolbox.tiangolo.com"]'
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = []
+    
+    
+    @validator("KAKAO_APP_KEY", pre=True)
+    def get_kakao_app_key(cls, v: Optional[str], values: Dict[str, Any]) -> Any:
+        if not v:
+            return values.get("KAKAO_APP_KEY")
+        return v
 
     @validator("BACKEND_CORS_ORIGINS", pre=True)
     def assemble_cors_origins(cls, v: Union[str, List[str]]) -> Union[List[str], str]:
