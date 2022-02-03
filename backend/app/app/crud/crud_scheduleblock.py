aa5047d3a175eea8adac250e6e308b62f9f91f6d
Kim Dongwoo <andanend2847@gmail.com>
2022-02-03 23:04:55 +0900

    wip
    test

--- backend/app/app/crud/crud_scheduleblock.py
+++ backend/app/app/crud/crud_scheduleblock.py
@@ -12,11 +12,11 @@ from app.core.security import create_uuid
 
 
 class CRUDScheduleblock(CRUDBase[ScheduleBlock, ScheduleBlockCreate, ScheduleBlockUpdate]):
-    def create(self, db: Session, *, obj_in: ScheduleBlockCreate) -> ScheduleBlock:
+    def create(self, db: Session, *, obj_in: ScheduleBlockCreate, user_id: str) -> ScheduleBlock:
         db_obj = ScheduleBlock(
             id=create_uuid(),
             table_id=obj_in.table_id,
-            user_id=obj_in.user_id,
+            user_id=user_id,
             start_datetime=obj_in.start_datetime,
             end_datetime=obj_in.end_datetime,
             label=obj_in.label
@@ -26,6 +26,23 @@ class CRUDScheduleblock(CRUDBase[ScheduleBlock, ScheduleBlockCreate, ScheduleBlo
         db.refresh(db_obj)
         return db_obj
     
+    def create_all(self, db: Session, *, objs_in: List[ScheduleBlockCreate], user_id: str) -> List[ScheduleBlock]:
+        db_objs = []
+        
+        for obj in objs_in:
+            db_objs.append(
+                ScheduleBlock(
+                    id=create_uuid(),
+                    table_id=obj.table_id,
+                    user_id=user_id,
+                    start_datetime=obj.start_datetime,
+                    end_datetime=obj.end_datetime,
+                    label=obj.label
+                )
+            )
+        db.add_all(db_objs)
+        db.commit()
+        return db_objs
     
     def get_all(self, db:Session, table_id:str):
         db_obj = db.query(self.model.user_id, self.model.start_datetime, self.model.end_datetime).filter(self.model.table_id==table_id).all()
