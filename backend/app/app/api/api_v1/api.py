c296ae51838c94fca900289ad52e86138ed2e1af
Kim Dongwoo <andandend2847@gmail.com>
2022-02-08 21:02:30 +0900

    refactor: Delete User endpoint
    안쓰는 /user 관련 코드 삭제

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,11 +1,7 @@
 from fastapi import APIRouter
 
-from app.api.api_v1.endpoints import users, root
+from app.api.api_v1.endpoints import root
 
 api_router = APIRouter()
 
 api_router.include_router(root.router, tags=["root"])
-# api_router.include_router(login.router, tags=["login"])
-# api_router.include_router(users.router, prefix="/users", tags=["users"])
-# api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
-# api_router.include_router(items.router, prefix="/items", tags=["items"])
