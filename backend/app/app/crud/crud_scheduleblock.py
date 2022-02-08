b97cdd925d0d30cf8ddea30ef479d0511454ac0a
Kim Dongwoo <andandend2847@gmail.com>
2022-02-08 23:38:00 +0900

    feat: Change scheduleblock data model
    time 저장 모델 구조 변경함
    변경에 따른 crud, endpoint 로직 변경함

--- backend/app/app/crud/crud_scheduleblock.py
+++ backend/app/app/crud/crud_scheduleblock.py
@@ -1,7 +1,9 @@
 from pydoc import describe
 from typing import Any, Dict, Optional, Union, List
+from fastapi import HTTPException
 from sqlalchemy import table
 
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
@@ -17,41 +19,32 @@ class CRUDScheduleblock(
     def create(
         self, db: Session, *, obj_in: ScheduleBlockCreate, user_id: str
     ) -> ScheduleBlock:
-        db_obj = ScheduleBlock(
-            id=create_uuid(),
-            table_id=obj_in.table_id,
-            user_id=user_id,
-            start_time=obj_in.start_time,
-            end_time=obj_in.end_time,
-            day=obj_in.day,
-            label=obj_in.label,
-        )
+        obj_in_data = jsonable_encoder(obj_in)
+        if obj_in.start_time not in range(0, 23) or obj_in.end_time not in range(0, 23):
+            raise HTTPException(400, detail="Time data must be in 0~23")
+        if obj_in.start_minute not in range(0, 59) or obj_in.end_minute not in range(
+            0, 59
+        ):
+            raise HTTPException(400, detail="Minute data must be in 0~59")
+
+        if obj_in.day not in range(0, 6):
+            raise HTTPException(
+                400, detail="Day data must be in 0~6, 0: Sunday, 6: Saturday"
+            )
+
+        db_obj = ScheduleBlock(id=create_uuid(), user_id=user_id, **obj_in_data)
         db.add(db_obj)
         db.commit()
         db.refresh(db_obj)
         return db_obj
 
     def get_all(self, db: Session, table_id: str):
-        db_obj = (
-            db.query(
-                self.model.user_id,
-                self.model.start_time,
-                self.model.end_time,
-                self.model.day,
-            )
-            .filter(self.model.table_id == table_id)
-            .all()
-        )
+        db_obj = db.query(self.model).filter(self.model.table_id == table_id).all()
         return db_obj
 
     def get_all_by_user_id(self, db: Session, table_id: str, user_id: str):
         db_obj = (
-            db.query(
-                self.model.user_id,
-                self.model.start_time,
-                self.model.end_time,
-                self.model.day,
-            )
+            db.query(self.model)
             .filter(self.model.table_id == table_id, self.model.user_id == user_id)
             .all()
         )
