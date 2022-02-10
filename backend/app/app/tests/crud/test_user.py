c3f3e6bd342a594fa33aec88171e21050fe26ddf
Kim Dongwoo <andandend2847@gmail.com>
2022-02-10 23:20:26 +0900

    refactor: Delete unused import module

--- backend/app/app/tests/crud/test_user.py
+++ backend/app/app/tests/crud/test_user.py
@@ -2,7 +2,6 @@ from fastapi.encoders import jsonable_encoder
 from sqlalchemy.orm import Session
 
 from app import crud
-from app.core.security import verify_password
 from app.schemas.user import UserCreate, UserUpdate
 from app.tests.utils.utils import random_email, random_lower_string
 
