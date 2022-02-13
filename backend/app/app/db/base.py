f93b871e9d01cac371fcb318ca30893d8b9b20b9
Kim Dongwoo <andandend2847@gmail.com>
2022-02-13 22:02:41 +0900

    test: Add ignore lint note for model import

--- backend/app/app/db/base.py
+++ backend/app/app/db/base.py
@@ -4,4 +4,4 @@ from app.db.base_class import Base  # noqa
 from app.models.scheduleblock import ScheduleBlock  # noqa
 from app.models.timetable import TimeTable  # noqa
 from app.models.user import User  # noqa
-from app.models.color import Color
+from app.models.color import Color  # noqa
