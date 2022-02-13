2ac7602154ed9d5422d936cc52e5b102d00ad036
Kim Dongwoo <andandend2847@gmail.com>
2022-02-13 23:36:55 +0900

    feat: Add new constraint on scheduleblock table
    동일 시간에 중복되지 않도록 제약조건 추가함

--- backend/app/app/models/scheduleblock.py
+++ backend/app/app/models/scheduleblock.py
@@ -1,6 +1,13 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import Column, DateTime, ForeignKey, SmallInteger, String
+from sqlalchemy import (
+    Column,
+    DateTime,
+    ForeignKey,
+    SmallInteger,
+    String,
+    UniqueConstraint,
+)
 from sqlalchemy.orm import relationship
 from sqlalchemy.sql import func
 
@@ -23,3 +30,16 @@ class ScheduleBlock(Base):
     created_at = Column(DateTime(timezone=True), server_default=func.now())
     timetable = relationship("TimeTable", back_populates="scheduleblocks")
     user = relationship("User", backref="scheduleblocks")
+
+    __table_args__ = (
+        UniqueConstraint(
+            "table_id",
+            "user_id",
+            "start_time",
+            "start_minute",
+            "end_time",
+            "end_minute",
+            "day",
+            name="uc_time_table",
+        ),
+    )
