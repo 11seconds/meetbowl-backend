c06ea3d37c2108ff0887715ae8efe855d2772091
Kim Dongwoo <andandend2847@gmail.com>
2022-03-06 22:29:30 +0900

    feat: Implement create/delete all scheduleblock by day
    특정 유저의 특정 요일 스케쥴블럭을 모두 생성하거나 삭제하는 기능 개발

--- backend/app/app/crud/crud_scheduleblock.py
+++ backend/app/app/crud/crud_scheduleblock.py
@@ -136,5 +136,62 @@ class CRUDScheduleblock(
 
         return db_obj
 
+    def create_day_by_user_id(
+        self, db: Session, table_id: str, user_id: str, day: int
+    ) -> List[ScheduleBlock]:
+        db_objs = [
+            ScheduleBlock(
+                id=create_uuid(),
+                table_id=table_id,
+                user_id=user_id,
+                start_time=start_time,
+                start_minute=0,
+                end_time=start_time,
+                end_minute=59,
+                day=day,
+            )
+            for start_time in range(24)
+        ]
+
+        db.add_all(db_objs)
+        db.commit()
+        db_obj = (
+            db.query(
+                self.model.id,
+                self.model.start_time,
+                self.model.start_minute,
+                self.model.end_time,
+                self.model.end_minute,
+                self.model.day,
+                self.model.label,
+                self.model.user_id,
+                self.model.table_id,
+                Color.hex.label("color"),
+                User.nickname,
+            )
+            .outerjoin(self.model.user)
+            .outerjoin(User.color)
+            .filter(self.model.table_id == table_id, User.id == user_id)
+            .all()
+        )
+        return db_obj
+
+    def delete_day_by_user_id(
+        self, db: Session, table_id: str, user_id: str, day: int
+    ) -> Any:
+        db_obj = (
+            db.query(self.model)
+            .filter(
+                self.model.table_id == table_id,
+                self.model.user_id == user_id,
+                self.model.day == day,
+            )
+            .delete()
+        )
+
+        db.commit()
+
+        return db_obj
+
 
 scheduleblock = CRUDScheduleblock(ScheduleBlock)
